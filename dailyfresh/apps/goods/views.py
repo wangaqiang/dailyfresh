ff67a5d39912177ff922b0e131ae98ec860a9e52
wangzhengqiang <wang893000986@163.com>
2019-09-05 16:27:02 +0800

    首页内容的获取和展示

--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import View
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 
 # Create your views here.
-def index(request):
+class IndexView(View):
     '''首页'''
-    return render(request, 'index.html')
+    def get(self, request):
+        '''显示首页'''
+        # 获取商品的种类信息
+        types = GoodsType.objects.all()
+
+        # 获取首页轮播商品信息
+        goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+        # 获取首页促销活动信息
+        promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+        # 获取首页分类商品展示信息
+        for type1 in types: # GoodsType
+            # 获取type种类首页分类商品的图片展示信息
+            image_banners = IndexTypeGoodsBanner.objects.filter(type=type1, display_type=1).order_by('index')
+            # 获取type种类首页分类商品的文字展示信息
+            title_banners = IndexTypeGoodsBanner.objects.filter(type=type1, display_type=0).order_by('index')
+
+            # 动态给type增加属性，分别保存首页分类商品的图片展示信息和文字展示信息
+            type1.image_banners = image_banners
+            type1.title_banners = title_banners
+            
+
+
+        # 获取用户购物车中商品的数目
+        cart_count = 0
+
+        # 组织模板上下文
+        context = {'types':types,
+                   'goods_banners':goods_banners,
+                   'promotion_banners':promotion_banners,
+                   'cart_count':cart_count}
+
+        return render(request, 'index.html', context)
