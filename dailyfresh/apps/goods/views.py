9831cb3b3249472a2ea298579686f54584ee156b
wangzhengqiang <wang893000986@163.com>
2019-09-08 20:27:47 +0800

    利用nginx实现首页页面的静态化

--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
-from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from goods.models import GoodsType, IndexGoodsBanner, GoodsSKU, IndexPromotionBanner, IndexTypeGoodsBanner
+from order.models import OrderGoods
 from django_redis import get_redis_connection
 
 # Create your views here.
 class IndexView(View):
     '''首页'''
     def get(self, request):
+
         '''显示首页'''
         # 获取商品的种类信息
         types = GoodsType.objects.all()
@@ -46,3 +49,42 @@ class IndexView(View):
                    'cart_count':cart_count}
 
         return render(request, 'index.html', context)
+
+# /goods/商品id
+class DetailView(View):
+    '''详情页'''
+    def get(self, request, goods_id):
+        '''显示详情页'''
+        try:
+            sku = GoodsSKU.objects.get(id=goods_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return redirect(reverse('goods:index'))
+
+        # 获取商品的分类信息
+        types = GoodsType.objects.all()
+
+        # 获取商品的评论信息
+        sku_orders = OrderGoods.objects.filter(sku=sku).exclude(comment='')
+
+        # 获取新品信息
+        new_skus = GoodsSKU.objects.filter(type=sku.type).order_by('-create_time')
+
+        # 获取用户购物车中商品的数目
+        user = request.user
+        cart_count = 0
+        if user.is_authenticated():
+            # 用户已经登录
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d'%user.id
+            cart_count = conn.hlen(cart_key)
+            
+        # 组织模板上下文
+        context = {'sku':sku,
+                   'types':types,
+                   'sku_orders':sku_orders,
+                   'new_skus':new_skus,
+                   'cart_count':cart_count}
+
+        # 使用模板
+        return render(request, 'detail.html', context)
